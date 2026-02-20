8ef51101d88433aaca077ce4f5173cfad349873f
Karina Sedova <karina.sed@yandex.ru>
2026-02-20 12:08:55 +0300

    feat: setup admin board

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
 # Register your models here.
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ('username', 'is_staff', 'is_active', 'last_login')
+    list_filter = ('is_staff', 'is_active')
+
+    search_fields = ('username',)
+    
+    ordering = ('username',)
+    
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Login history', {'fields': ('last_login',)}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'password'),
+        }),
+    )
