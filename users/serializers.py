3d2b8c0aa7870cafa62dde422c78ca39bb583b99
Karina Sedova <karina.sed@yandex.ru>
2026-02-23 12:59:03 +0300

    feat: add seerializer for login history

--- users/serializers.py
+++ users/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
+from .models import LoginHistory
 
 User = get_user_model()
 
@@ -16,3 +17,11 @@ class RegisterSerializer(serializers.ModelSerializer):
             password=validated_data['password']
         )
         return user
+
+
+class LoginHistorySerializer(serializers.ModelSerializer):
+    login_time = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S")
+
+    class Meta:
+        model = LoginHistory
+        fields = ('id', 'login_time')
