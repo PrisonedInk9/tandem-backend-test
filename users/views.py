60089cf4446e214b516587b45ff18234d6b37d70
Karina Sedova <karina.sed@yandex.ru>
2026-02-23 14:10:00 +0300

    feat: add pagination to login history

--- users/views.py
+++ users/views.py
@@ -2,12 +2,19 @@ from django.shortcuts import render
 from rest_framework import generics
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.pagination import PageNumberPagination
 from django.contrib.auth import get_user_model
 from .serializers import RegisterSerializer, LoginHistorySerializer
 from .models import LoginHistory
 
 # Create your views here.
 User = get_user_model()
+
+class LoginHistoryPagination(PageNumberPagination):
+    page_size = 15
+    page_size_query_param = 'page_size'
+    max_page_size = 50
+
 
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
@@ -29,6 +36,7 @@ class CustomTokenObtainPairView(TokenObtainPairView):
 class UserLoginHistoryView(generics.ListAPIView):
     serializer_class = LoginHistorySerializer
     permission_classes = (IsAuthenticated,)
+    pagination_class = LoginHistoryPagination
 
     def get_queryset(self):
         return LoginHistory.objects.filter(user=self.request.user)
